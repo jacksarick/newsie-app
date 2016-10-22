5b6fe29b42467ad317da9749ee82bc914f3a0d4e
Jack Sarick <jacksarick@gmail.com>
2016-10-22 13:58:35 -0400

    Save Sat 22 Oct 2016 13:58:35 EDT

--- app.js
+++ app.js
@@ -34,3 +34,75 @@ client.on("invalid-state", function(cmd) {
 client.on("locked", function(cmd) {
         console.log("Current command has not finished yet. You tried calling " + cmd);
 });
+
+client.on("login", function(status, rawdata) {
+
+    if (status) {
+
+        console.log("LOGIN/PASS success");
+        client.list();
+
+    } else {
+
+        console.log("LOGIN/PASS failed");
+        client.quit();
+
+    }
+});
+
+// Data is a 1-based index of messages, if there are any messages
+client.on("list", function(status, msgcount, msgnumber, data, rawdata) {
+
+    if (status === false) {
+
+        console.log("LIST failed");
+        client.quit();
+
+    } else {
+
+        console.log("LIST success with " + msgcount + " element(s)");
+
+        if (msgcount > 0)
+            client.retr(1);
+        else
+            client.quit();
+
+    }
+});
+
+client.on("retr", function(status, msgnumber, data, rawdata) {
+
+    if (status === true) {
+
+        console.log("RETR success for msgnumber " + msgnumber);
+        client.quit();
+
+    } else {
+
+        console.log("RETR failed for msgnumber " + msgnumber);
+        client.quit();
+
+    }
+});
+
+client.on("dele", function(status, msgnumber, data, rawdata) {
+
+    if (status === true) {
+
+        console.log("DELE success for msgnumber " + msgnumber);
+        client.quit();
+
+    } else {
+
+        console.log("DELE failed for msgnumber " + msgnumber);
+        client.quit();
+
+    }
+});
+
+client.on("quit", function(status, rawdata) {
+
+    if (status === true) console.log("QUIT success");
+    else console.log("QUIT failed");
+
+});
