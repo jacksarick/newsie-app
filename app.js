5a41faae7cf2e2a8c5ebfc966cf65a428d5ddb9a
Jack Sarick <jacksarick@gmail.com>
2016-10-22 13:36:06 -0400

    Got imap-simple working

---
+++ app.js
@@ -0,0 +1,36 @@
+var imaps = require('imap-simple');
+ 
+var config = {
+	imap: {
+		user: 'test@newsie.club',
+		password: 'mypassword1',
+		host: 'imap.gmail.com',
+		port: 993,
+		tls: true,
+		authTimeout: 3000
+	}
+};
+ 
+imaps.connect(config).then(function (connection) {
+ 
+	return connection.openBox('INBOX').then(function () {
+		var searchCriteria = [
+			'UNSEEN'
+		];
+ 
+		var fetchOptions = {
+			bodies: ['HEADER', 'TEXT'],
+			markSeen: false
+		};
+ 
+		return connection.search(searchCriteria, fetchOptions).then(function (results) {
+			var subjects = results.map(function (res) {
+				return res.parts.filter(function (part) {
+					return part.which === 'HEADER';
+				})[0].body.subject[0];
+			});
+ 
+			console.log(subjects);
+		});
+	});
+});
